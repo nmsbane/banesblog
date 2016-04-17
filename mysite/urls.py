d8a06083016ee5ce29506ae487ae7937ef121bdc
nmsbane <madhusudhan638@gmail.com>
2016-04-17 15:02:35 +0530

    change the root url to map to /blog url

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
 
@@ -11,8 +12,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^$', RedirectView.as_view(url='/blog')),
 )
